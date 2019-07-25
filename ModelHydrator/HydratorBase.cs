8851f2fa90d932b46a3a8db43506075e69bf27d3
Charles Lampman <Charles.Lampman@outlook.com>
2019-07-25 12:48:07 -0500

    Add project files.

---
+++ ModelHydrator/HydratorBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelHydrator
+{
+    public class HydratorBase
+    {
+        private readonly Type _modelType;
+
+        public HydratorBase(Type modelType)
+        {
+            _modelType = modelType;
+        }
+
+        public object GetInstance()
+        {
+            var instance = Activator.CreateInstance(_modelType);
+
+            return instance;
+        }
+    }
+}
---
+++ Test/HydratorBaseTest.cs
@@ -0,0 +1,31 @@
+﻿using ModelHydrator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Xunit;
+
+namespace Test
+{
+
+    public class HydratorBaseTest
+    {
+        [Fact]
+        public void GetInstance_StringLength_PopulatedProperty()
+        {
+            var hydrator = new HydratorBase(typeof(TestModel));
+            var model = (TestModel)hydrator.GetInstance();
+
+            Assert.InRange(model.TextField.Length, TestModel.USERNAME_MIN, TestModel.USERNAME_MAX);
+        }
+    }
+
+    public class TestModel
+    {
+        public const int USERNAME_MAX = 10;
+        public const int USERNAME_MIN = 5;
+
+        [StringLength(USERNAME_MAX, MinimumLength = USERNAME_MIN)]
+        public string TextField { get; set; }
+    }
+}
